434032bbaf28366d992ff7ec4ae1f9cc60920c6d
Артур Лагунов <a.lagunov@ktsstudio.ru>
2025-04-03 13:45:25 +0400

    -

--- svc/api/nx_api/svc/auth/ia/register.py
+++ svc/api/nx_api/svc/auth/ia/register.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import nx_api.dto as d
 import nx_api.repo as r
 import nx_api.infra.db.models as m
+from nx_api.svc.auth.ia.verify_email import VerifyEmailIA
 from nx_api.svc.auth.pwd_crypt import IPwdCrypt
 
 
@@ -11,6 +12,7 @@ class RegisterIA:
     _user_repo: r.User
     _db_sess: AsyncSession
     _crypt: IPwdCrypt
+    _verify_email: VerifyEmailIA
 
     async def __call__(self, dto: d.NewUser) -> m.User:
         user = await self._user_repo.add(
@@ -18,4 +20,5 @@ class RegisterIA:
             **dto.model_dump(exclude={"password"}),
         )
         await self._db_sess.commit()
+        await self._verify_email(email=user.email)
         return user
